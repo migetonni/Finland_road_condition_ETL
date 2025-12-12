519252459d7030ce212976f252518124643807cf
migetonni <mixu03@gmail.com>
2025-12-12 14:57:21 +0200

    changed transformation so more forecasts available

--- transform/transform_df.py
+++ transform/transform_df.py
@@ -14,13 +14,13 @@ def transform_track_df(df):
 
     #drop rows with old information since we only want the latest road condition data
 
-    df = df.query("forecast_name == '2h' or forecast_name == '0h'")
+    df = df.query("forecast_name == '2h' or forecast_name == '0h' or forecast_name '4h' or forecast_name == '6h'")
 
     df["road_number"] = df["section_id"].str.split("_").str[0].astype(int)
     print(df["road_number"])
 
     def road_classifier(num):
-        if num < 39:
+        if num < 40:
             return "mainRoad"
         elif num > 39 and num < 100:
             
